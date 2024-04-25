321fd8757bbcdd06adb0f28ece42357651959432
Neo Fiesta <newytpremium001@gmail.com>
2024-04-26 00:36:47 +0530

    Keyfinder

--- controllers/keyFinder.js
+++ controllers/keyFinder.js
@@ -3,6 +3,7 @@ import fetch from 'node-fetch'; // Import fetch for making HTTP requests
 
 async function findKey(url) {
     try {
+        console.log("Finding Keys for ", url)
         url = `https://extractapi.xyz/drm.php?v=${url}`
         const response = await fetch(url, {
             credentials: "omit",
@@ -10,12 +11,6 @@ async function findKey(url) {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
                 "Accept": "application/json", // Request JSON response
                 "Accept-Language": "en-US,en;q=0.5",
-                "Upgrade-Insecure-Requests": "1",
-                "Sec-Fetch-Dest": "document",
-                "Sec-Fetch-Mode": "navigate",
-                "Sec-Fetch-Site": "none",
-                "Sec-Fetch-User": "?1",
-                "X-Forwarded-For": "144.208.40.186"
             },
             method: "GET",
             mode: "cors"
@@ -26,6 +21,7 @@ async function findKey(url) {
         }
 
         const data = await response.json(); // Convert response to JSON format
+        console.log("Got Keys ", data.keys[0])
         return data.keys[0]; // Return keys[0]
     } catch (error) {
         throw new Error('Error:', error);
