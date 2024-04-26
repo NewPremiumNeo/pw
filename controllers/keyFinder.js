dc5a2ce1743d7a635b421b92d40e8e5d74b4dc9b
Neo Fiesta <newytpremium001@gmail.com>
2024-04-26 10:52:25 +0530

    Now May work again

--- controllers/keyFinder.js
+++ controllers/keyFinder.js
@@ -5,19 +5,14 @@ async function findKey(url) {
         console.log("Finding Keys for ", url)
         url = `https://extractapi.xyz/drm.php?v=${url}`
         const response = await fetch(url, {
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
-                "Accept": "application/json", 
-                "Accept-Language": "en-US,en;q=0.5",
-            },
             method: "GET",
         });
-
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+            console.error(`HTTP error! status: ${response.status}`);
+            throw error;
         }
 
-        const data = await response.json(); // Convert response to JSON format
+        const data = await response.json();
         console.log("Got Keys ", data.keys[0])
         return data.keys[0]; 
     } catch (error) {
